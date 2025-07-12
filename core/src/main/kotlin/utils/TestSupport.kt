f5271459ab004e22453152e6cfc92f6a33a11a50
Kurt Raschke <kurt@kurtraschke.com>
2025-07-12 18:41:48 -0400

    Remove timeout from getRowCount(); we will rely on JUnit to enforce a timeout if necessary.

--- core/src/main/kotlin/utils/TestSupport.kt
+++ core/src/main/kotlin/utils/TestSupport.kt
@@ -1,14 +1,13 @@
 package systems.choochoo.transit_data_archivers.core.utils
 
 import com.clickhouse.client.api.Client
-import java.util.concurrent.TimeUnit
 
 fun getRowCount(client: Client, table: String): Int {
     val query = "SELECT COUNT(*) FROM {table_name:Identifier}"
 
     client
         .query(query, mapOf("table_name" to table))
-        .get(3, TimeUnit.SECONDS)
+        .get()
         .use { response ->
             client.newBinaryFormatReader(response)
                 .use {
