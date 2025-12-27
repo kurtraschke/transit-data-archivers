fb3a32bc2ba3b3f46bb5ae2dc115efd338a68478
Kurt Raschke <kurt@kurtraschke.com>
2025-12-27 12:56:46 -0500

    fix overzealous cleanup

--- common/src/main/kotlin/listeners/SchedulerShutdownListener.kt
+++ common/src/main/kotlin/listeners/SchedulerShutdownListener.kt
@@ -1,12 +1,13 @@
 package systems.choochoo.transit_data_archivers.common.listeners
 
 import io.github.oshai.kotlinlogging.slf4j.toKLogger
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import org.quartz.listeners.SchedulerListenerSupport
 import java.util.concurrent.CountDownLatch
 
 @Singleton
-class SchedulerShutdownListener : SchedulerListenerSupport() {
+class SchedulerShutdownListener @Inject constructor() : SchedulerListenerSupport() {
     val schedulerShutdownLatch = CountDownLatch(1)
 
     private val log = super.log.toKLogger()
